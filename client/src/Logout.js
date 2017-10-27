937903e7eb0baf9014e6f1a05042b28725dbe5e6
Steven Peters <steve@Jennas-MacBook-Air.local>
2017-10-26 22:05:07 -0700

    Committing individualized mern auth boilerplate for Mnemo

---
+++ client/src/Logout.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Logout extends Component {
+  constructor(props) {
+    super(props)
+    this.handleLogout = this.handleLogout.bind(this)
+  }
+
+  handleLogout(e) {
+    e.preventDefault()
+    this.props.logout()
+  }
+
+  render() {
+    return (
+      <a onClick={this.handleLogout}>Logout</a>
+    );
+  }
+}
+
+export default Logout;
