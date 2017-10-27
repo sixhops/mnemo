0130b8a910184f11fe8d00ca2c8cc2187406fcd1
Steven Peters <steve@Jennas-MacBook-Air.local>
2017-10-27 16:46:35 -0700

    Adding a path back in

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
