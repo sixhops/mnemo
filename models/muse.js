ef93dfc52a0ddcc26477f0397f14d49a6bcdfe58
sixhops <sixhops@gmail.com>
2017-11-13 15:10:52 -0800

    Adding muse model and CRUD routes

---
+++ models/muse.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var museSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 80
+  },
+  summary: {
+    type: String,
+    minlength: 0,
+    maxlength: 150
+  },
+  dateAdded: {
+    type: Date,
+    required: true
+  }
+});
